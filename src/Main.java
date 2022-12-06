da633c0001adcaf961318ebab9970ca9c1d652d4
ddevos <ddevos@JFC-CL3WJD2>
2022-12-06 13:49:45 +0100

    serveur TCP

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
     
-    //*coucou c'est dodo bordel
+    try {
+    	serverSocket = new ServerSoket(4444);
+    }
+    catch(IOException e) {
+    	System.out.println("Could not listen on port 4444");
+    	System.exit(-1);
+    }
+    
+    Socket clientSocket=null;
+    try {
+    	clientSocket=serverSocket.accept();
+    }
+    catch(IOException e) {
+    	System.out.println("Accept failed on port 4444");
+    	System.exit(-1);
+    }
 }
